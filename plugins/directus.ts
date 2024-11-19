bbaf7ac8e9121055dca2623b2a80cabab5d50d48
Jairus Joer <jairus.joer@hero-software.de>
2024-11-19 17:45:36 +0100

    Improve implementation of retrieveSchemaSnapshot

--- plugins/directus.ts
+++ plugins/directus.ts
@@ -1,4 +1,11 @@
-import { authentication, createDirectus, rest, schemaDiff, schemaSnapshot, type SchemaSnapshotOutput } from '@directus/sdk';
+import {
+  authentication,
+  createDirectus,
+  rest,
+  schemaDiff,
+  schemaSnapshot,
+  type SchemaSnapshotOutput,
+} from '@directus/sdk';
 import type { PublicSchema } from '~/directus/collections/schema';
 import type { RuntimeConfig } from '@nuxt/schema';
 
@@ -26,14 +33,14 @@ export default defineNuxtPlugin(async () => {
 
 const createPublicClient = (runtimeConfig: RuntimeConfig) => {
   return createDirectus<PublicSchema>(runtimeConfig.public.url).with(rest());
-}
+};
 
 const createAdminClient = async (runtimeConfig: RuntimeConfig) => {
   const client = createDirectus(runtimeConfig.public.url).with(authentication()).with(rest());
   await client.login(runtimeConfig.admin.email, runtimeConfig.admin.password);
 
   return client;
-}
+};
 
 const retrieveSchemaSnapshot = async (client: Awaited<ReturnType<typeof createAdminClient>>) => {
   const { readFile, writeFile } = await import('fs/promises');
@@ -44,17 +51,22 @@ const retrieveSchemaSnapshot = async (client: Awaited<ReturnType<typeof createAd
   } catch {
     schema = await client.request(schemaSnapshot());
     await writeFile('directus/schema.json', JSON.stringify(schema, null, 2));
-    console.info('\x1b[36m%s\x1b[0m', 'Directus', 'created schema snapshot')
+    console.info('Directus', 'created schema snapshot');
   }
 
-  const { diff } = await client.request(schemaDiff(schema));
+  try {
+    const { diff } = await client.request(schemaDiff(schema));
 
-  if (diff) {
-    schema = await client.request(schemaSnapshot());
-    await writeFile('directus/schema.json', JSON.stringify(schema, null, 2));
-    console.info('\x1b[36m%s\x1b[0m', 'Directus', 'updated schema snapshot')
+    if (diff) {
+      schema = await client.request(schemaSnapshot());
+      await writeFile('directus/schema.json', JSON.stringify(schema, null, 2));
+      console.info('Directus', 'updated schema snapshot');
+    }
+  } catch (error: any) {
+    for (const { message } of error.errors) {
+      console.warn('Directus', 'rejected schema snapshot:', message);
+    }
   }
 
   return schema;
 };
-
