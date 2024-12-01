b2e97db99fdb550077aac65b57b3e46376cbc92d
Jairus Joer <hello@jairusjoer.com>
2024-12-01 17:35:28 +0100

    Add theme support for semantic tokens

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,3 @@
-<script setup lang="ts">
-const variants = ['bold', 'subtle', 'outlined', 'ghost'];
-const colors = ['brand', 'neutral', 'informative', 'successful', 'cautionary', 'destructive'];
-</script>
-
 <template>
-  <!-- <WelcomeSplash /> -->
-<div class="p-2">
-  <div v-for="variant in variants" class="p-2 space-x-4">
-    <Button
-      v-for="color in colors"
-      :variant
-      :color
-      disabled
-    >
-      Button Label
-    </Button>
-  </div>
-</div>
+  <WelcomeSplash />
 </template>
